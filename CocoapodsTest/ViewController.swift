54b39602abcc74e3df068efa44dec8c39216a30f
Martin Kubista <mkubista@kswr.cz>
2018-09-03 21:14:06 +0200

    Initial Commit

---
+++ CocoapodsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocoapodsTest
+//
+//  Created by Martin Kubista on 03/09/2018.
+//  Copyright © 2018 mkubista. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
